6d35138d0d2699989279ab0975f62a241ff39ea0
ChairmaDuraiCape <127489359+ChairmaDuraiCape@users.noreply.github.com>
2024-10-09 16:53:03 +0530

    build: config

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    basePath: "/connect",
-    assetPrefix: "/connect/",
     typescript: {
         ignoreBuildErrors: true,
-    },
-    async redirects() {
-        return [
-            {
-                source: "/connect/",
-                destination: "/",
-                permanent: true,
-            },
-        ];
     },
 };
 
---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// Export the default middleware from next-auth
+export { default } from 'next-auth/middleware';
+
+export function middleware(request: NextRequest) {
+  // Split the URL path into parts
+  const urlPathName = request.nextUrl.pathname;
+
+  const pathParts = urlPathName.split('/');
+
+  const fisrtIndex = pathParts[1];
+
+  const removedData = [
+    '_next',
+    'icons',
+    'images',
+    'api',
+    'connect',
+  ];
+
+  if (!removedData.includes(fisrtIndex)) {
+    if (urlPathName !== '/') {
+      const removedDataSplit = request.url.split('/');
+      removedDataSplit.splice(3, 0, 'connect');
+      const newUrlString = removedDataSplit.join('/');
+      return NextResponse.redirect(new URL(newUrlString, request.url));
+    }
+  }
+}
+
+export const config = {
+  matcher: '/:path*',
+};
