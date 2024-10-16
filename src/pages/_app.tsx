59f6f99ac7631577f8523647346850a561551738
ChairmaDuraiCape <127489359+ChairmaDuraiCape@users.noreply.github.com>
2024-10-16 12:53:28 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,7 +20,7 @@ const sora = Sora({
 export default function App({ Component, pageProps }: AppProps) {
   const { store } = wrapper.useWrappedStore(pageProps);
   return (
-    <SessionProvider basePath={CONSTANTT.ASSESTPATH + "/api/auth"}>
+    <SessionProvider basePath={CONSTANT.ASSESTPATH + "/api/auth"}>
       <Provider store={store}>
         <section className={`${sora.variable} font-sora h-[100vh] w-full flex`}>
           <Layouts>
