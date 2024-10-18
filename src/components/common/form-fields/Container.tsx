1e8a3e62dac42bfa87a180118cac3b349c3cc953
Monisha-g27 <135809809+Monisha-g27@users.noreply.github.com>
2024-10-18 17:42:09 +0530

    commit

--- src/components/common/form-fields/Container.tsx
+++ src/components/common/form-fields/Container.tsx
@@ -1,16 +1,9 @@
 import * as React from "react";
-import { Container } from "@mui/material";
+// import { Container } from "@mui/material";
 type LayoutsTypes = {
   children: React.ReactNode;
 };
 
 export default function CustomContainer({ children }: LayoutsTypes) {
-  return (
-    <Container
-      maxWidth={"lg"}
-      sx={{ maxWidth: "1269px", width: { xl: "95%", lg: "95%" } }}
-    >
-      {children}
-    </Container>
-  );
+  return <div className="container mx-auto">{children}</div>;
 }
